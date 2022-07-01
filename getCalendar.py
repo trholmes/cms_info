7180f68220f714aa38202e80f5129e54d11de4bc
Tova Holmes <tholmes@cern.ch>
2022-07-01 21:01:37 +0200

    Added approvals to upcoming events.

--- getCalendar.py
+++ getCalendar.py
@@ -21,43 +21,75 @@ def getURL(ev):
         if url in ev: return urls[url]
     return default_url
 
+def getEvents(ev_data):
+    events = []
+    this_event = {}
+    for line in ev_data:
+        if line.startswith("BEGIN:VEVENT"):
+            this_event = {}
+        elif line.startswith("END:VEVENT"):
+            events.append(this_event)
+        else:
+            try:
+                this_event[line.split(":")[0]]=line.split(":")[1].strip()
+            except:
+                continue
+    return events
+
+def isSoon(event, days_from_now = 21, days_ago = 5):
+    try:
+        try:
+            edate = datetime.datetime.strptime(event["DTSTART;VALUE=DATE"], '%Y%m%d').date()
+        except:
+            edate = datetime.datetime.strptime(event["DTSTART"].split("T")[0], '%Y%m%d').date()
+    except Exception as e:
+        return False, None
+    if (edate - today).days < days_from_now and (today-edate).days < days_ago:
+        return True, edate
+    return False, edate
+
+## Grab events from the CMS calendar
+
 cal_file = "https://www.google.com/calendar/ical/dnfcb10nk2fj96tcippfoprpak%40group.calendar.google.com/public/basic.ics"
 tmp_cal = "/afs/cern.ch/user/t/tholmes/useful_files/cron_scripts/cms-info/cal.ics"
 json_file = "/eos/user/t/tholmes/www/tova/other/cal.json"
 data = urllib2.urlopen(cal_file)
 output = data.read()
-
 with open(tmp_cal, 'w') as f:
     f.write(output)
-
 with open(tmp_cal, 'r') as f:
     ev_data = f.readlines()
-
-events = []
-this_event = {}
-for line in ev_data:
-    if line.startswith("BEGIN:VEVENT"):
-        this_event = {}
-    elif line.startswith("END:VEVENT"):
-        events.append(this_event)
-    else:
-        try:
-            this_event[line.split(":")[0]]=line.split(":")[1].strip()
-        except:
-            continue
-
-
+events = getEvents(ev_data)
 events_to_write = []
 for event in events:
-    try:
-        edate = datetime.datetime.strptime(event["DTSTART;VALUE=DATE"], '%Y%m%d').date()
-    except:
-        continue
-    if (edate - today).days < 21 and (edate - today).days > -5:
+    is_soon, edate = isSoon(event)
+    if is_soon:
         if event["SUMMARY"] not in ["FB", "MB", "MB/FB"]:
             url = getURL(event["SUMMARY"])
             ev_data = {"name": event["SUMMARY"], "url": url, "date": edate.strftime("%d %b")+": ", "ndays": (edate - today).days}
             events_to_write.append(ev_data)
+
+
+
+## Also add approvals
+
+cal_file = "https://calendar.google.com/calendar/ical/teir7hdjshmfgvcl2jmopq186g%40group.calendar.google.com/public/basic.ics"
+tmp_cal = "/afs/cern.ch/user/t/tholmes/useful_files/cron_scripts/cms-info/cal.ics"
+json_file = "/eos/user/t/tholmes/www/tova/other/cal.json"
+data = urllib2.urlopen(cal_file)
+output = data.read()
+with open(tmp_cal, 'w') as f:
+    f.write(output)
+with open(tmp_cal, 'r') as f:
+    ev_data = f.readlines()
+events = getEvents(ev_data)
+for event in events:
+    is_soon, edate = isSoon(event, 21, 5)
+    if is_soon:
+        ev_data = {"name": event["SUMMARY"], "url": event["DESCRIPTION"], "date": edate.strftime("%d %b")+": ", "ndays": (edate - today).days}
+        events_to_write.append(ev_data)
+
+## Check if empty and package up in a json
 
 if len(events_to_write)==0:
     ev_data = {"name": "None", "url": "/cms-internal/cms-calendar/", "date": "", "ndays": 0}
@@ -68,3 +100,4 @@ sorted_events = sorted(events_to_write, key=lambda item: item["ndays"])
 f = open(json_file, "w")
 json.dump(sorted_events, f)
 f.close()
+
