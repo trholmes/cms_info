f9f4d4e42e271938bbbf6ab3695fcf8679f074d3
Tova Holmes <tholmes@cern.ch>
2022-07-01 22:17:13 +0200

    Also added CERN seminars, fixed URL parsing issue

--- getCalendar.py
+++ getCalendar.py
@@ -5,18 +5,18 @@ import datetime
 today = datetime.date.today()
 
 urls = {
-        "CMS week": "https://indico.cern.ch/category/154/",
-        "Tracker week": "https://indico.cern.ch/category/1080/",
-        "LHCC": "https://indico.cern.ch/category/3427/",
-        "Induction": "https://indico.cern.ch/category/14397/",
-        "O&C": "https://indico.cern.ch/category/1382/",
-        "RRB": "https://indico.cern.ch/category/12324/",
-        "P2UG": "https://indico.cern.ch/category/11282/",
+        "cms week": "https://indico.cern.ch/category/154/",
+        "tracker week": "https://indico.cern.ch/category/1080/",
+        "lhcc": "https://indico.cern.ch/category/3427/",
+        "induction": "https://indico.cern.ch/category/14397/",
+        "o&c": "https://indico.cern.ch/category/1382/",
+        "rrb": "https://indico.cern.ch/category/12324/",
+        "p2ug": "https://indico.cern.ch/category/11282/",
         }
 
 def getURL(ev):
     default_url = "https://indico.cern.ch/category/6803/"
-    if ev in urls: return urls[ev]
+    if ev.lower() in urls: return urls[ev.lower()]
     for url in urls:
         if url in ev: return urls[url]
     return default_url
@@ -31,28 +31,32 @@ def getEvents(ev_data):
             events.append(this_event)
         else:
             try:
-                this_event[line.split(":")[0]]=line.split(":")[1].strip()
+                this_event[line.split(":")[0]]=":".join(line.split(":")[1:]).strip()
             except:
                 continue
     return events
 
 def isSoon(event, days_from_now = 21, days_ago = 5):
     try:
+        time_str = ""
+        for val in event:
+            if "DTSTART" in val: time_str = val
         try:
-            edate = datetime.datetime.strptime(event["DTSTART;VALUE=DATE"], '%Y%m%d').date()
+            edate = datetime.datetime.strptime(event[time_str], '%Y%m%d').date()
         except:
-            edate = datetime.datetime.strptime(event["DTSTART"].split("T")[0], '%Y%m%d').date()
+            edate = datetime.datetime.strptime(event[time_str].split("T")[0], '%Y%m%d').date()
     except Exception as e:
         return False, None
     if (edate - today).days < days_from_now and (today-edate).days < days_ago:
         return True, edate
     return False, edate
 
+tmp_cal = "/afs/cern.ch/user/t/tholmes/useful_files/cron_scripts/cms-info/cal.ics"
+json_file = "/eos/user/t/tholmes/www/tova/other/cal.json"
+
 ## Grab events from the CMS calendar
 
 cal_file = "https://www.google.com/calendar/ical/dnfcb10nk2fj96tcippfoprpak%40group.calendar.google.com/public/basic.ics"
-tmp_cal = "/afs/cern.ch/user/t/tholmes/useful_files/cron_scripts/cms-info/cal.ics"
-json_file = "/eos/user/t/tholmes/www/tova/other/cal.json"
 data = urllib2.urlopen(cal_file)
 output = data.read()
 with open(tmp_cal, 'w') as f:
@@ -69,13 +73,9 @@ for event in events:
             ev_data = {"name": event["SUMMARY"], "url": url, "date": edate.strftime("%d %b")+": ", "ndays": (edate - today).days}
             events_to_write.append(ev_data)
 
-
-
 ## Also add approvals
 
 cal_file = "https://calendar.google.com/calendar/ical/teir7hdjshmfgvcl2jmopq186g%40group.calendar.google.com/public/basic.ics"
-tmp_cal = "/afs/cern.ch/user/t/tholmes/useful_files/cron_scripts/cms-info/cal.ics"
-json_file = "/eos/user/t/tholmes/www/tova/other/cal.json"
 data = urllib2.urlopen(cal_file)
 output = data.read()
 with open(tmp_cal, 'w') as f:
@@ -88,6 +88,21 @@ for event in events:
     if is_soon:
         ev_data = {"name": event["SUMMARY"], "url": event["DESCRIPTION"], "date": edate.strftime("%d %b")+": ", "ndays": (edate - today).days}
         events_to_write.append(ev_data)
+
+## Add in CERN EP and LHC seminars
+for cal_file in ["https://indico.cern.ch/category/3249/events.ics", "https://indico.cern.ch/category/3247/events.ics"]:
+    data = urllib2.urlopen(cal_file)
+    output = data.read()
+    with open(tmp_cal, 'w') as f:
+        f.write(output)
+    with open(tmp_cal, 'r') as f:
+        ev_data = f.readlines()
+    events = getEvents(ev_data)
+    for event in events:
+        is_soon, edate = isSoon(event, 10, 1)
+        if is_soon:
+            ev_data = {"name": "CERN Seminar: " + event["SUMMARY"], "url": event["URL"], "date": edate.strftime("%d %b")+": ", "ndays": (edate - today).days}
+            events_to_write.append(ev_data)
 
 ## Check if empty and package up in a json
 
