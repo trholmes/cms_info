02ae441744108fe99f9262bdffca876484356480
Tova Holmes <tholmes@cern.ch>
2024-07-25 16:58:59 +0200

    Updated old SSH to use lxplus8 nodes.

--- getDBs.sh
+++ getDBs.sh
@@ -16,15 +16,15 @@ cd /afs/cern.ch/user/c/cmswww/cms_info
 
 python3 ./getDB.py 'https://icms.cern.ch/tools-api/restplus/org_chart/tenures?exclude_past=true' > ${loc}tenures_raw.json
 
-# as long as CINCO is behind the old SSO, use lxplus7:
-ssh -q lxplus7 "cd cms_info; python3 ./getOldDB.py 'https://cms-mgt-conferences.web.cern.ch/conferences/conferences_list_short.aspx' > ${loc}cinco_raw.json "
+# as long as CINCO is behind the old SSO, use lxplus8:
+ssh -q lxplus8 "cd cms_info; python3 ./getOldDB.py 'https://cms-mgt-conferences.web.cern.ch/conferences/conferences_list_short.aspx' > ${loc}cinco_raw.json "
 
 # check if there was a problem, if so, wait a bit and retry
 grep 'Error: Cannot authenticate to: https://cms-mgt-conferences.web' /eos/project-c/cmsweb/www/icmssecr/cms-info/cinco_raw.json >/dev/null 2>&1
 ret=$?
 if [ "${ret}" == "0" ]; then
     sleep 10
-    ssh -q lxplus7 "cd cms_info; python3 ./getOldDB.py 'https://cms-mgt-conferences.web.cern.ch/conferences/conferences_list_short.aspx' > ${loc}cinco_raw.json "
+    ssh -q lxplus8 "cd cms_info; python3 ./getOldDB.py 'https://cms-mgt-conferences.web.cern.ch/conferences/conferences_list_short.aspx' > ${loc}cinco_raw.json "
 fi
 
 python3 ./getDB.py 'https://icms.cern.ch/tools-api/restplus/cadi/xeb_report?xeb_report_period=14' > ${loc}cadi_raw.json
